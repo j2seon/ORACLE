63134bf114805a1f455921712693075ca47fccf2
j2seon <ddj04323@gmail.com>
2023-04-21 15:29:22 +0900

    20230421

--- 03_그룹함수와단일행함수.sql
+++ 03_그룹함수와단일행함수.sql
@@ -118,6 +118,7 @@ SELECT LTRIM('5289GREEDY','123456789') FROM DUAL;
 SELECT RTRIM('12345000000', '0') FROM DUAL;
 SELECT RTRIM('GREEDY5289', '123456789') FROM DUAL;
 SELECT RTRIM('ACABACCGREEDYAAAAAB', 'ABC') FROM DUAL;
+SELECT RTRIM('ACABACCGREEDYAAAAAB', 'ABC') FROM DUAL;
 
 -- TRIM : 주어진 컬럼이나 문자열의 앞/뒤에 지정한 문자를 제거
 SELECT TRIM('      GREEDY    ') FROM DUAL;
@@ -341,11 +342,334 @@ SELECT
       , EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM HIRE_DATE) 근무년수
     FROM EMPLOYEE;
 
+-- 형변환 함수
+-- TO_CHAR(날짜, [포멧]) : 날짜형 데이터를 문자형 데이터로 변경
+-- TO_CHAR(숫자, [포멧]) : 숫자형 데이터를 문자형 데이터로 변경
+-- '9999999999'자리수를 나타낸다.
+-- '000000'은 자리수만큼 0을 나타냄
+-- 'L9999999'  ￦표시
+-- 숫자가 크면 #으로 표시됌
+SELECT TO_CHAR(123) FROM DUAL;
+SELECT TO_CHAR(1234, '9999999999') FROM DUAL;
+SELECT TO_CHAR(1234, '000000') FROM DUAL;
+SELECT TO_CHAR(1234, 'L9999') FROM DUAL;
+SELECT TO_CHAR(1232443, '$9,999,999') FROM DUAL;
+SELECT TO_CHAR(1324, '00,000') FROM DUAL;
+SELECT TO_CHAR(1324, '999') FROM DUAL;
+
+
+-- 직원 테이블에서 사원명 ,급여 조회, 급여는 '\9,000,000'형식으로 표시하세요
+SELECT 
+        EMP_NAME
+      , TO_CHAR(SALARY, 'L99,999,999')
+    FROM EMPLOYEE;
+    
+-- 날짜 데이터 포멧 적용시에도 TO_CHAR 함수적용
+SELECT TO_CHAR(SYSDATE, 'PM HH24:MI:SS') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'AM HH:MI:SS') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'MON DY, YYYY') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD DAY') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'YYYY/fmMM/DD DAY') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'YEAR, Q') || '분기' FROM DUAL;
+
+SELECT 
+        EMP_NAME
+      , HIRE_DATE
+    FROM EMPLOYEE;
+
+SELECT 
+        EMP_NAME
+      , TO_CHAR(HIRE_DATE, 'YYYY-MM-DD')입사일
+    FROM EMPLOYEE;
+    
+SELECT 
+        EMP_NAME
+      , TO_CHAR(HIRE_DATE, 'YYYY/MM/DD HH24:MI:SS')입사일
+    FROM EMPLOYEE;
 
 
 
+-- 오늘 날짜에 대해 년도4자리, 년도2자리
+SELECT 
+        TO_CHAR(SYSDATE, 'YYYY')
+      , TO_CHAR(SYSDATE, 'RRRR')
+      , TO_CHAR(SYSDATE, 'YY')
+      , TO_CHAR(SYSDATE, 'RR')
+    FROM DUAL;
+
+-- RR과 YY의 차이
+-- RR은 두자리년도를 네자리로 바꿀 때
+-- 바꿀 년도가 50년미만 2000년을 적용
+-- 50년 이상이면 1900년 적용
+
+-- 년도를 바꿀 때(TO_DATE) 사용시 Y를 적용하면
+-- 현재 세기(2000년)가 적용된다.
+
+-- R은 50년 이상이면 이전 세기(1900년)
+-- 50년 미만이면 현재세기(2000년) 적용
+
+SELECT 
+        TO_CHAR(TO_DATE('980630','YYMMDD'), 'YYYY-MM-DD DY')
+    FROM DUAL;
+
+SELECT 
+        TO_CHAR(TO_DATE('980630','RRMMDD'), 'RRRR-MM-DD')
+    FROM DUAL;
+
+-- 오늘 날짜에서 월만 출력
+SELECT 
+        TO_CHAR(SYSDATE, 'MM')
+      , TO_CHAR(SYSDATE, 'MONTH')
+      , TO_CHAR(SYSDATE, 'MON')
+      , TO_CHAR(SYSDATE, 'RM')
+    FROM DUAL; 
+
+-- 오늘 날짜에서 일만 출력
+SELECT 
+        TO_CHAR(SYSDATE, '"1년 기준" DDD "일 째"')
+      , TO_CHAR(SYSDATE, '"달기준" DD "일 째"')
+      , TO_CHAR(SYSDATE, '"주 기준" D "일 째"')
+    FROM DUAL; 
 
 
+/* 직원 테이블에서 이름, 입사일 조회
+   입사일에 포멧 적용
+   '2023년 4월 21일(금)'형식으로 출력처리하세요
+*/
+SELECT 
+        EMP_NAME
+      , TO_CHAR(HIRE_DATE, 'YYYY"년" FMMM"월" DD"일("DY")"') 입사일
+    FROM EMPLOYEE;
+
+-- TO_DATE : 문자 혹은 숫자형 데이터를 날짜형 데이터로 변환하여 리턴
+-- TO_DATE(문자형데이터, [포멧])
+-- 문자형 데이터를 날짜로 변경한다.
+
+-- TO_DATE(숫자, [포멧]
+
+SELECT 
+        TO_DATE('20200101','RRRRMMDD')
+    FROM DUAL;
+
+SELECT 
+        TO_CHAR(TO_DATE('20200101','RRRRMMDD'), 'RRRR, MON')
+    FROM DUAL;
+
+SELECT 
+        TO_CHAR(TO_DATE('041030 143000', 'RRMMDD HH24MISS'), 'DD/MON/RRRR HH:MI:SS PM')
+    FROM DUAL;
+    
+-- 직원 테이블에서 2000년도 이후에 입사한 사원의 사번, 이름, 입사일을 조회하세요
+SELECT
+        EMP_ID
+      , EMP_NAME
+      , HIRE_DATE
+    FROM EMPLOYEE 
+--   WHERE HIRE_DATE > '20000101'; -- 자동형변환됌(문자열을 날짜로 자동 형변환된다)
+--   WHERE HIRE_DATE > 20000101; -- 숫자는 날짜로 자동형변환 안된다.
+   WHERE HIRE_DATE > TO_DATE(20000101,'RRRRMMDD');
+
+-- TO_NUMBER(문제데이터, [포멧]) : 문자데이터를 숫자로 리턴
+SELECT TO_NUMBER('123456789') FROM DUAL;
+
+-- 자동 형변환
+SELECT '123' + '456' FROM DUAL;
+-- 숫자로 된 문자열만 자동형변환 가능
+SELECT '123'+ '456aA' FROM DUAL; 
+
+SELECT 
+        EMP_NAME
+      , HIRE_DATE
+    FROM EMPLOYEE
+   WHERE HIRE_DATE = '90/02/06'; -- 자동 형번환
+   
+-- 사번이 홀수인 직원의 정보
+SELECT 
+        *
+    FROM EMPLOYEE
+--   WHERE MOD(EMP_ID, 2) = 1;  -- 자동형변환
+   WHERE MOD(TO_NUMBER(EMP_ID), 2) = 1;
+
+-- 문자가 붙은상태라서 X
+SELECT
+        '1,000,000' + '5,000,000'
+    FROM DUAL;
+    
+SELECT
+        TO_NUMBER('1,000,000','99,999,999') + TO_NUMBER('5,000,000','99,999,999')
+    FROM DUAL;
+
+/*
+    날짜, 숫자 -> 문자(TO_CHAR)
+    문자 -> 날짜(TO_DATE)
+    문자 -> 숫자(TO_NUMBER)
+*/
+
+-- 직원 테이블에서 사원번호가 201인 사원의 이름, 주민번호 앞자리, 주민번호 뒷자리,
+-- 주민번호 앞자리와 뒷자리의 합을 조회하세요
+-- 단, 자동형변환 사용하지 않고 조회
+
+SELECT 
+        EMP_NAME
+      , EMP_NO
+      , SUBSTR(EMP_NO, 1, 6) 앞부분
+      , SUBSTR(EMP_NO, 8) 뒷부분
+      , TO_NUMBER(SUBSTR(EMP_NO, 1, 6))+ TO_NUMBER(SUBSTR(EMP_NO, 8)) 결과
+    FROM EMPLOYEE
+   WHERE EMP_ID = TO_CHAR(201);
+
+-- NULL처리 함수
+-- NVL(컬럼명, 컬럼값이 NULL일때 바꿀 값)
+SELECT 
+        EMP_NAME
+      , BONUS
+      , NVL(BONUS, 0)
+    FROM EMPLOYEE;
+    
+-- NVL2(컬럼명, 바꿀값1, 바꿀값2)
+-- 해당 컬럼이 값이 있으면 바꿀값1로 변경,
+-- 해당 컬럼이 NULL이면 바꿀값2로 변경
+
+-- 직원정보에서 보너스포인트가 NULL인 직원은 0.5로
+-- 보너스 포인트가 NULL이 아닌 경우 0.7로 변경하여 조회
+
+SELECT 
+        EMP_NAME
+      , BONUS
+      , NVL2(BONUS, 0.7, 0.5)
+    FROM EMPLOYEE;
+
+-- 선택함수
+-- 여러가지 경우에 선택할 수 있는 기능을 제공한다.
+-- DECODE(계산식 | 컬럼명, 조건값1, 선택값1, 조건값2, 선택값2....)
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , EMP_NO
+      , DECODE(SUBSTR(EMP_NO, 8, 1), '1', '남', '2', '여') 성별
+    FROM EMPLOYEE;
+
+-- 마지막 인자로 조건값 없이 선택값을 작성하면
+-- 아무것도 해당하지 않을 때 마지막에 작성한 선택값을 무조건 선택한다.
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , EMP_NO
+      , DECODE(SUBSTR(EMP_NO, 8, 1), '1', '남', '여') 성별
+    FROM EMPLOYEE;
+
+-- 직원의 급여를 인상하고자 한다.
+-- 직급코드가 J7인 직원은 급여의 10%를 인상하고
+-- 직급코드가 J6인 직원은 급여의 15%를 인상하고
+-- 직급코드가 J5인 직원은 급여의 20%를 인상한다.
+-- 그 외 직급의 직원은 5%만 인상한다.
+-- 직원 테이블에서 사원명, 직급코드, 급여, 인상급여를 조회하세요
+
+SELECT 
+        EMP_ID
+      , JOB_CODE
+      , SALARY
+      , DECODE(JOB_CODE, 'J7', SALARY * 1.1,
+                         'J6', SALARY * 1.15,
+                         'J5', SALARY * 1.2,
+                               SALARY * 1.05) 인상급여
+    FROM EMPLOYEE;
+      
+/* CASE 
+     WHEN 조건식 THEN 결과값
+     WHER 조건식 THEN 결과값
+     ELSE 결과값
+     END
+  */    
+SELECT 
+        EMP_ID
+      , JOB_CODE
+      , SALARY
+      , CASE
+          WHEN JOB_CODE = 'J7' THEN SALARY * 1.1
+          WHEN JOB_CODE = 'J6' THEN SALARY * 1.15
+          WHEN JOB_CODE = 'J5' THEN SALARY * 1.2
+          ELSE SALARY * 1.05
+        END AS 인상급여
+    FROM EMPLOYEE;
+            
+/*
+    사번, 사원명, 급여를 조회하고 
+    급여가 500만원 초과이면 '고급'
+    급여가 300~500만원이면 '중급'
+    그 이하는 '초급'으로 출력하고 별칭은 구분으로
+*/
+      
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , SALARY
+      , CASE
+          WHEN SALARY >5000000 THEN '고급'
+          WHEN SALARY BETWEEN 3000000 AND 5000000 THEN '중급'
+          ELSE '초급'
+        END AS 구분
+    FROM EMPLOYEE;
+    
+    
+-- 함수 연습 문제 
+/*1. 직원명과 주민번호를 조회함
+  단, 주민번호 9번째 자리부터 끝까지는 '*'문자로 채운다.
+  예 : 홍길동 771120-1******
+*/     
+SELECT 
+        EMP_NAME
+      , RPAD(SUBSTR(EMP_NO, 1, 8),14,'*') AS 주민번호
+    FROM EMPLOYEE;
+    
+/*
+2. 직원명, 직급코드, 연봉(원) 조회
+   단, 연봉은 ￦57,000,000 으로 표시되게 함
+   연봉은 보너스포인트가 적용된 1년치 급여이다
+*/
+
+
+/* 3. 부서코드가 D5, D9인 직원들 중에서 2004년도에 입사한 직원의 직원수를 조회 */
+
+
+
+/*4. 직원명, 입사일, 입사한 달의 근무일수 조회
+  단, 주말도 포함함
+*/
+
+
+
+/* 5. 직원명, 부서코드, 생년월일, 나이(만) 조회
+     단, 생년월일은 주민번호에서 추출해서, 
+     ㅇㅇ년 ㅇㅇ월 ㅇㅇ일로 출력되게 함.
+     나이는 주민번호에서 추출해서 날짜데이터로 변환한 다음, 계산함
+*/
+
+
+
+/* 6. 직원들의 입사일로 부터 년도만 가지고, 각 년도별 입사인원수를 구하시오.
+      아래의 년도에 입사한 인원수를 조회하시오.
+      => to_char, decode, COUNT 사용
+
+        -------------------------------------------------------------
+        전체직원수   2001년   2002년   2003년   2004년
+        -------------------------------------------------------------
+*/
+
+/*7.  부서코드가 D5이면 총무부, D6이면 기획부, D9이면 영업부로 처리하시오.
+      단, 부서코드가 D5, D6, D9 인 직원의 정보만 조회함
+      => case 사용
+*/
+
+
+      
+      
+      
+      
+      
+      
+      
+      
       
       
       
---
+++ 04_GROUPBYHAVING그리고ORDERBY.sql
@@ -0,0 +1,295 @@
+-- GROUP
+
+
+-- ORDER BY : SELECT한 컬럼을 가지고 정렬을 할 때 사용한다.
+-- ORDER BY 컬럼명 | 컬럼별칭 | 컬럼 나열 순번 [ASC] | DESC
+
+-- ORDER BY 컬럼명 정렬방식, 컬럼명 정렬방식, 컬럼명 정렬방식...
+--> 첫번재 기준으로 하는 컬럼에 대해 정렬하고 같은 값들에 대해 두번째 기준으로 하는 컬럼명에 대하여 다시정렬
+
+-- SELECT 구문의 맨 마지막에 위치, 실행순서도 맨 마지막
+
+/*
+    수행순서
+    5 : SELECT 컬럼명 AS 별칭, 계산식, 함수식
+    1 : FROM 참조할 테이블명
+    2 : WHERE 컬럼명 | 함수식 비교연산자 비교값
+    3 : GROUP BY 그룹을 묶을 컬럼명 
+    4 : HAVING 그룹함수식 비교연산자 비교값
+    6 : ORDER BY 컬럼명 | 별칭 | 컬럼순선정렬 방식 [NULLS FIRST | LAST]
+*/
+
+SELECT 
+        DEPT_CODE
+      , COUNT(*) 사원수
+    FROM EMPLOYEE
+   GROUP BY DEPT_CODE;
+   
+SELECT 
+        DEPT_CODE
+      , JOB_CODE
+      , SUM(SALARY)
+      , COUNT(*)
+    FROM EMPLOYEE
+   GROUP BY DEPT_CODE, JOB_CODE;
+
+-- 직원 테이블에서 부서 코드별 그룹을 지정하여 
+-- 부서코드, 그룹별 급여의 합계, 그룹별 급여의 평균(정수처리), 인원수 조회
+-- 부서코드순으로 오름차순 정렬하세요
+
+SELECT 
+        DEPT_CODE
+      , SUM(SALARY) 합계
+      , FLOOR(AVG(SALARY)) 평균
+      , COUNT(*) 인원수
+    FROM EMPLOYEE
+   GROUP BY DEPT_CODE
+--   ORDER BY DEPT_CODE ASC;
+--   ORDER BY DEPT_CODE DESC NULLS FIRST;
+   ORDER BY 합계 ASC NULLS FIRST;
+
+-- 직원테이블에서 직급코드 보너스를 받는 사원 수를 조회하여
+-- 직급코드 순으로 오름차순 정렬하세요
+SELECT 
+        JOB_CODE
+      , COUNT(*)
+    FROM EMPLOYEE
+   WHERE BONUS IS NOT NULL
+   GROUP BY JOB_CODE
+   ORDER BY JOB_CODE;
+      
+SELECT 
+        JOB_CODE
+      , COUNT(*)
+    FROM EMPLOYEE
+   WHERE BONUS IS NOT NULL
+   GROUP BY JOB_CODE
+   ORDER BY JOB_CODE;
+      
+-- 직원 테이블에서 주민번호의 8번째 자리를 조회하여 1이면 남, 2면 여로 결과를 조회하고
+-- 성별별 급여 평균(점수처리), 급여합계, 인원수를 조회한 뒤 인원수로 내림차순 정렬
+SELECT 
+        DECODE(SUBSTR(EMP_NO, 8, 1),'1','남','여') 성별
+      , FLOOR(AVG(SALARY)) 평균
+      , SUM(SALARY) 합계
+      , COUNT(*) 인원수
+    FROM EMPLOYEE
+   GROUP BY DECODE(SUBSTR(EMP_NO, 8, 1),'1','남','여')
+   ORDER BY 인원수 DESC;
+
+-- HAVING절 : 그룹함수로 구해올 그룹에 대해 조건을 설정할 때 사용
+-- HAVING 컬럼명 | 함수식 비교연산자 비교값
+
+SELECT 
+        DEPT_CODE
+      , FLOOR(AVG(SALARY)) 평균
+    FROM EMPLOYEE
+   WHERE SALARY > 300000
+   GROUP BY DEPT_CODE
+   ORDER BY DEPT_CODE;
+   
+SELECT 
+        DEPT_CODE
+      , FLOOR(AVG(SALARY)) 평균
+    FROM EMPLOYEE
+   GROUP BY DEPT_CODE
+   HAVING FLOOR(AVG(SALARY)) > 3000000
+   ORDER BY DEPT_CODE;
+
+-- 부서별 그룹의 급여 합계 중 9백만원을 초과하는 부서 코드와 급여 합계 조회
+SELECT 
+        DEPT_CODE
+      , SUM(SALARY) 합계
+    FROM EMPLOYEE
+   GROUP BY DEPT_CODE
+   HAVING SUM(SALARY) > 9000000;
+
+-- 급여 합계가 가장 많은 부서의 부서코드와 급여 합계를 구하세요
+SELECT
+        MAX(SUM(SALARY))
+    FROM EMPLOYEE
+   GROUP BY DEPT_CODE;
+   
+SELECT 
+        DEPT_CODE
+      , SUM(SALARY)
+    FROM EMPLOYEE
+   GROUP BY DEPT_CODE
+   HAVING SUM(SALARY) = 17700000;
+
+
+SELECT 
+        DEPT_CODE
+      , SUM(SALARY)
+    FROM EMPLOYEE
+   GROUP BY DEPT_CODE
+   HAVING SUM(SALARY) = (SELECT MAX(SUM(SALARY))
+                           FROM EMPLOYEE
+                           GROUP BY DEPT_CODE);
+
+-- 집계함수
+-- ROLLUP 함수 : 그룹별로 중간 집계 처리하는 함수 
+-- GROUP BY 절에서만 사용하는 함수 
+-- 그룹별로 묶여진 값에 대한 중간 집계와 총 집계를 구할 때 사용
+-- 그룹별로 계산된 결과값들에 대한 총 집계가 자동으로 추가된다.
+SELECT 
+        JOB_CODE
+      , SUM(SALARY)
+    FROM EMPLOYEE
+   GROUP BY ROLLUP(JOB_CODE)
+   ORDER BY JOB_CODE;
+
+-- CUBE 함수 : 그룹별 산출한 결과를 집계하는 함수
+SELECT 
+        JOB_CODE
+      , SUM(SALARY)
+    FROM EMPLOYEE
+   GROUP BY CUBE(JOB_CODE)
+   ORDER BY JOB_CODE;
+
+-- 인자로 전달한 그룹 중에서 가장 먼저 지정한 그룹별 함계와 총 합계를 구하는 함수
+SELECT 
+        DEPT_CODE
+      , JOB_CODE
+      , SUM(SALARY)
+    FROM EMPLOYEE
+   GROUP BY ROLLUP(DEPT_CODE, JOB_CODE)
+   ORDER BY JOB_CODE;
+ 
+-- 그룹으로 지정된 모든 그룹에 대한 집계와 총 합계를 구하는 함수
+SELECT 
+        DEPT_CODE
+      , JOB_CODE
+      , SUM(SALARY)
+    FROM EMPLOYEE
+   GROUP BY CUBE(DEPT_CODE, JOB_CODE)
+   ORDER BY JOB_CODE;
+ 
+-- GROUPING 함수 : ROLLUP이나 CUBE에 의한 산출물이 인자로 전달받은 컬럼의 
+--                집합 산출물이면 0 반환, 아니면 1 반환
+SELECT 
+        DEPT_CODE
+      , JOB_CODE
+      , SUM(SALARY)
+      , GROUPING(DEPT_CODE) "직급별 그룹 묶은 상태"
+      , GROUPING(JOB_CODE) "부서별그룹 묶인상태"
+    FROM EMPLOYEE
+   GROUP BY CUBE(DEPT_CODE, JOB_CODE)
+   ORDER BY DEPT_CODE;
+
+SELECT 
+        DEPT_CODE
+      , JOB_CODE
+      , SUM(SALARY)
+      , CASE
+          WHEN GROUPING(DEPT_CODE) = 0 AND GROUPING(JOB_CODE) = 1 THEN '부서별합계'
+          WHEN GROUPING(DEPT_CODE) = 1 AND GROUPING(JOB_CODE) = 0 THEN '직급별합계'
+          WHEN GROUPING(DEPT_CODE) = 0 AND GROUPING(JOB_CODE) = 0 THEN '그룹별합계'
+          ELSE '총합계'
+       END AS 구분  
+    FROM EMPLOYEE
+   GROUP BY CUBE(DEPT_CODE, JOB_CODE)
+   ORDER BY DEPT_CODE;
+
+-- SET OPERATION(집합연산)
+-- UNION : 여러개의 쿼리 결과를 하나로 합치는 연산자
+--         중복된 영역을 제외하여 하나로 합친다.
+
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , DEPT_CODE
+      , SALARY
+    FROM EMPLOYEE
+   WHERE DEPT_CODE = 'D5'
+   UNION
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , DEPT_CODE
+      , SALARY
+    FROM EMPLOYEE
+   WHERE SALARY > 3000000;
+
+
+-- UNION ALL : 여러개의 쿼리 결과를 하나로 합치는 연산자
+--             
+ 
+ SELECT 
+        EMP_ID
+      , EMP_NAME
+      , DEPT_CODE
+      , SALARY
+    FROM EMPLOYEE
+   WHERE DEPT_CODE = 'D5'
+   UNION ALL
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , DEPT_CODE
+      , SALARY
+    FROM EMPLOYEE
+   WHERE SALARY > 3000000;
+
+SELECT 
+        DEPT_CODE
+      , JOB_CODE
+      , SUM(SALARY)
+    FROM EMPLOYEE
+   GROUP BY ROLLUP(DEPT_CODE, JOB_CODE)
+   UNION
+SELECT 
+        ''
+      , JOB_CODE
+      , SUM(SALARY)
+    FROM EMPLOYEE
+   GROUP BY ROLLUP(DEPT_CODE, JOB_CODE);
+
+-- INTERSECT : 여러개의 SELECT한 결과에서 공통 부분만 결과로추출
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , DEPT_CODE
+      , SALARY
+  FROM EMPLOYEE
+ WHERE DEPT_CODE = 'D5'
+INTERSECT
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , DEPT_CODE
+      , SALARY
+  FROM EMPLOYEE
+ WHERE SALARY > 3000000;
+ 
+-- MINUS : 선행 SELECT결과에서 다음 SELECT 결과와 겹치는 부분을 제외한 나머지 부분만 추출(수학에서의 차집합과 비슷하다)
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , DEPT_CODE
+      , SALARY
+  FROM EMPLOYEE
+ WHERE DEPT_CODE = 'D5'
+MINUS
+SELECT 
+        EMP_ID
+      , EMP_NAME
+      , DEPT_CODE
+      , SALARY
+  FROM EMPLOYEE
+ WHERE SALARY > 3000000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
